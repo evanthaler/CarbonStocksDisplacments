e66302ac6f3832672302d41adb95e4c26969e71c
evanthaler <thaler.evan@gmail.com>
2026-01-25 11:46:18 -0800

    update boxplot script and added figs

--- pythonScripts/SlopeDisplacementBoxplots.py
+++ pythonScripts/SlopeDisplacementBoxplots.py
@@ -17,48 +17,34 @@ tl27_nonpfslope = disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "0
 tl47_pfslope = disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "1"),'slope'].dropna()
 tl47_nonpfslope = disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "0"),'slope'].dropna()
 
-# pf27kde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "1"),'dhorizontal'].dropna())
-# nonpf27kde=gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "0"),'dhorizontal'].dropna())
-# pf27slopekde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "1"),'slope'].dropna())
-# nonpf27slopekde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "0"),'slope'].dropna())
-
-# pf47kde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "0"),'dhorizontal'].dropna())
-# nonpf47kde=gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "1"),'dhorizontal'].dropna())
-# pf47slopekde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "1"),'slope'].dropna())
-# nonpf47slopekde = gaussian_kde(disp_df.loc[(disp_df['Site'] == 'TL47') & (disp_df['pf'] == "0"),'slope'].dropna())
-
-disp27len = np.linspace(0,0.3,100)
-
-slope27len = np.linspace(
-    min(disp_df.loc[(disp_df['Site'] == 'TL27') ,'slope']),
-    max(disp_df.loc[(disp_df['Site'] == 'TL27') ,'slope']),
-    100)
 
 
-disp47len = np.linspace(0,0.3,100)
-
-slope47len = np.linspace(
-    min(disp_df.loc[(disp_df['Site'] == 'TL47') ,'slope']),
-    max(disp_df.loc[(disp_df['Site'] == 'TL47') ,'slope']),
-    100)
-
+colors = ['blue','lightblue','black','darkgray']
 fig,ax = plt.subplots(figsize=(5,5))
-#ax.boxplot([tl27_pfdisp,tl27_nonpfdisp,tl47_pfdisp,tl47_nonpfdisp],labels =['TL27 PF','TL27 NonPF','TL47 PF','TL47 NonPF'],showfliers=False )
-ax.boxplot([tl27_pfslope,tl27_nonpfslope,tl47_pfslope,tl47_nonpfslope],labels =['TL27 PF','TL27 NonPF','TL47 PF','TL47 NonPF'],showfliers=False )
+bplot = ax.boxplot(
+    [tl27_pfslope,tl27_nonpfslope,tl47_pfslope,tl47_nonpfslope],
+    tick_labels =['TL27 PF','TL27 NonPF','TL47 PF','TL47 NonPF'],
+    patch_artist=True,
+    showfliers=False )
+for patch, color in zip(bplot['boxes'], colors):
+    patch.set_facecolor(color)
+ax.tick_params(axis='x', labelrotation=45)
+ax.set_ylabel('Slope (degrees)')
+fig.show()
 
-plt.show()
-#ax.boxplot(disp_df.loc[(disp_df['Site'] == 'TL27') & (disp_df['pf'] == "1"),'dhorizontal'].dropna())
+colors = ['blue','lightblue','black','darkgray']
+fig,ax = plt.subplots(figsize=(5,5))
+bplot = ax.boxplot(
+    [tl27_pfdisp,tl27_nonpfdisp,tl47_pfdisp,tl47_nonpfdisp],
+    tick_labels =['TL27 PF','TL27 NonPF','TL47 PF','TL47 NonPF'],
+    patch_artist=True,
+    showfliers=False )
+for patch, color in zip(bplot['boxes'], colors):
+    patch.set_facecolor(color)
+ax.tick_params(axis='x', labelrotation=45)
+ax.set_ylabel('Horizontal displacement (m yr$^{-1}$)')
+fig.show()
 
 
-plt.show()
 
-# ax.plot(disp27len,pf27kde(disp27len),'--b',label ='TL27 PF')
-# ax.plot(disp27len,nonpf27kde(disp27len),'-b',label ='TL27 Non PF')
 
-# ax.plot(disp47len,pf47kde(disp47len),'--k',label ='TL47 PF')
-# ax.plot(disp47len,nonpf47kde(disp47len),'-k',label ='TL47 Non PF')
-# ax.set_xlabel('Horizontal displacement (m yr$^{-1}$)')
-# ax.set_ylabel('Probability distribution')
-# ax.set_ylim(0); ax.set_xlim(0)
-# ax.legend()
-# plt.show()
